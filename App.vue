e5122ac7389b6e4084c57a425e1edcdec4da956f
Mohona Chowdhury <89448559+MohonaJS@users.noreply.github.com>
2022-01-20 07:50:46 +0100

    conditional-bc-frontendulvecklare

--- App.vue
+++ App.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="app">
-    <form action="" @submit="check">
-      <label for="">User Name : </label><br>
-      <input v-model="username" type="text" name="" id="" placeholder="Your name"><br>
-      <label for="">Password : </label> <br>
-      <input v-model="password" type="password" placeholder="Your password"> <br>
-       <button >Submit</button>
-    </form>
-   
+  <!-- <button @click="shownum">Click me!!</button>
+  <h1 v-if="show == 1">FrontEnd developer</h1>
+  <h1 v-else-if="show == 2">Backend developer</h1> -->
+  <button @click="showRan">Click to choose!!</button>
+  <h1 v-if="show == 1">Frontend</h1>
+  <h1 v-else-if=" show == 2">Backend</h1>
+
+
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'App',
-  data(){return{  
-    username: '',
-    password: '',
-    givename: 'aveen',
-    givepassword: 1
+  /* data(){return{  
+    show: 0
   }},
-  methods:{  
-    check(e){  
-      e.preventDefault();
-      
-      if(this.username !== this.givename && this.password !== this.givepassword){
-       return
-      }else { alert('okay')}
-      
+  methods: {  
+    shownum(){  
+        this.show = Math.ceil(Math.random() * 2)
+    }
+  } */
+  data(){return{  
+    show : 0
+  }},
+  methods: {  
+    showRan(){  
+      this.show = Math.ceil(Math.random() * 2)
     }
   }
+  
+
+
+
 
   
 }
