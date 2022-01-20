9395da39ca5d57e632b64d519c9b3f85f5777ec0
Mohona Chowdhury <89448559+MohonaJS@users.noreply.github.com>
2022-01-20 07:49:44 +0100

    conditional bootcamp, fakedlogin

--- App.vue
+++ App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <TodoApp>
-      
-    </TodoApp>
+    <form action="" @submit="check">
+      <label for="">User Name : </label><br>
+      <input v-model="username" type="text" name="" id="" placeholder="Your name"><br>
+      <label for="">Password : </label> <br>
+      <input v-model="password" type="password" placeholder="Your password"> <br>
+       <button >Submit</button>
+    </form>
+   
   </div>
 </template>
 
 <script>
-import TodoApp from './components/TodoApp.vue'
-
 export default {
   name: 'App',
-  components: {
-    TodoApp
+  data(){return{  
+    username: '',
+    password: '',
+    givename: 'aveen',
+    givepassword: 1
+  }},
+  methods:{  
+    check(e){  
+      e.preventDefault();
+      
+      if(this.username !== this.givename && this.password !== this.givepassword){
+       return
+      }else { alert('okay')}
+      
+    }
   }
+
+  
 }
 </script>
 
