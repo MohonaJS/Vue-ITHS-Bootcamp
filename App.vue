b67dbfedcca8d0381518d99ab6f443c61fb779bf
Mohona Chowdhury <89448559+MohonaJS@users.noreply.github.com>
2022-01-20 07:51:25 +0100

    Conditional-BC-fruit

--- App.vue
+++ App.vue
@@ -1,42 +1,22 @@
 <template>
   <div id="app">
-  <!-- <button @click="shownum">Click me!!</button>
-  <h1 v-if="show == 1">FrontEnd developer</h1>
-  <h1 v-else-if="show == 2">Backend developer</h1> -->
-  <button @click="showRan">Click to choose!!</button>
-  <h1 v-if="show == 1">Frontend</h1>
-  <h1 v-else-if=" show == 2">Backend</h1>
-
-
+   <ol>
+     <li v-for="fruit in fruits" v-bind:key="fruit">
+       {{fruit}}
+     </li>
+   </ol>
   </div>
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
-  /* data(){return{  
-    show: 0
-  }},
-  methods: {  
-    shownum(){  
-        this.show = Math.ceil(Math.random() * 2)
-    }
-  } */
   data(){return{  
-    show : 0
-  }},
-  methods: {  
-    showRan(){  
-      this.show = Math.ceil(Math.random() * 2)
-    }
-  }
-  
-
-
-
-
+      fruits: [
+        'apel','lisi','mango','komola'
+      ]
+  }}
   
 }
 </script>
